35efead833355c11ec05cff400209958d5a91b2f
Matthias Menzi <menzi@imt.ch>
2021-02-17 10:33:04 +0100

    https://plugins.jetbrains.com/docs/intellij/reference-contributor.html#update-grammar-and-regenerate-the-parser Generated parser

--- src/main/gen/ch/imt/sdk/language/psi/impl/SimplePropertyImpl.java
+++ src/main/gen/ch/imt/sdk/language/psi/impl/SimplePropertyImpl.java
@@ -8,10 +8,9 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
 import static ch.imt.sdk.language.psi.SimpleTypes.*;
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import ch.imt.sdk.language.psi.*;
 
-public class SimplePropertyImpl extends ASTWrapperPsiElement implements SimpleProperty {
+public class SimplePropertyImpl extends SimpleNamedElementImpl implements SimpleProperty {
 
   public SimplePropertyImpl(@NotNull ASTNode node) {
     super(node);
@@ -35,6 +34,21 @@ public class SimplePropertyImpl extends ASTWrapperPsiElement implements SimplePr
   @Override
   public String getValue() {
     return SimplePsiImplUtil.getValue(this);
+  }
+
+  @Override
+  public String getName() {
+    return SimplePsiImplUtil.getName(this);
+  }
+
+  @Override
+  public PsiElement setName(String newName) {
+    return SimplePsiImplUtil.setName(this, newName);
+  }
+
+  @Override
+  public PsiElement getNameIdentifier() {
+    return SimplePsiImplUtil.getNameIdentifier(this);
   }
 
 }
