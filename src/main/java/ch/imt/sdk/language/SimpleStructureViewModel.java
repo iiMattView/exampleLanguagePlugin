df5ac6d367f06e0d9580440aed3ad98dd9696a44
Matthias Menzi <menzi@imt.ch>
2021-02-17 12:10:11 +0100

    https://plugins.jetbrains.com/docs/intellij/structure-view-factory.html#register-the-structure-view-factory

---
+++ src/main/java/ch/imt/sdk/language/SimpleStructureViewModel.java
@@ -0,0 +1,34 @@
+package ch.imt.sdk.language;
+
+import com.intellij.ide.structureView.StructureViewModel;
+import com.intellij.ide.structureView.StructureViewModelBase;
+import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
+import com.intellij.psi.PsiFile;
+import ch.imt.sdk.language.psi.SimpleFile;
+import org.jetbrains.annotations.NotNull;
+
+public class SimpleStructureViewModel extends StructureViewModelBase implements
+        StructureViewModel.ElementInfoProvider {
+
+    public SimpleStructureViewModel(PsiFile psiFile) {
+        super(psiFile, new SimpleStructureViewElement(psiFile));
+    }
+
+    @NotNull
+    public Sorter[] getSorters() {
+        return new Sorter[]{Sorter.ALPHA_SORTER};
+    }
+
+
+    @Override
+    public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
+        return false;
+    }
+
+    @Override
+    public boolean isAlwaysLeaf(StructureViewTreeElement element) {
+        return element instanceof SimpleFile;
+    }
+
+}
